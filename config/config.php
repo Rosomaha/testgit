3c429fb52028306acc04ca68c2e1052cf5cc7233
valeragalits <valeragalits@inbox.ru>
2017-11-25 09:27:23 +0200

    Base structure

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rosomaha
+ * Date: 11/25/17
+ * Time: 9:06 AM
+ */
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rosomaha
+ * Date: 11/25/17
+ * Time: 9:04 AM
+ */
